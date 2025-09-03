40f68ffcbbd07ae3d117a35d6187f0754206c1fc
itaru2622 <itaru2622@gmail.com>
2025-09-03 10:48:13 +0900

    cleanup: debug messages in hooks/*.sh

--- hooks/job-completed.sh
+++ hooks/job-completed.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-echo "script kicked >>>>>>>>>>>>>>>>>>>>>>>"
-echo "$0 $@"
-echo "script kicked <<<<<<<<<<<<<<<<<<<<<<<"
-
 _me=$0
 
-# exit 0 always even some error found, to passthrough steps...
+# exit 0 always even some error catched.
 trap catch ERR
 catch() {
-  echo "Trap ERR ${_me} ! But exit 0 for run job" 1>&2
+  code=$?
+  echo "catch ERR (${code}) @ ${_me} and ignored" 1>&2
   exit 0
 }
 
 rm -f ${STATUS_JOB_RUNNING}
-sDir=$(dirname $0)
 
-echo "sDir: ${sDir}-----------------------"
+sDir=$(dirname $0)
 for s in `ls ${sDir}/scripts.d/*-post*.sh |  sort -f`
 do
-   echo "${s} found then kick"
    ${s}
 done
 
--- hooks/job-started.sh
+++ hooks/job-started.sh
@@ -1,27 +1,21 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-echo "script kicked >>>>>>>>>>>>>>>>>>>>>>>"
-echo "$0 $@"
-echo "script kicked <<<<<<<<<<<<<<<<<<<<<<<"
-
 _me=$0
 
-# exit 0 always even some error found, to passthrough steps...
+# exit 0 always even some error found, to passthrough job/steps...
 trap catch ERR
 catch() {
-  echo "Trap ERR ${_me}! But exit 0 for run job" 1>&2
+  code=$?
+  echo "catch ERR (${code}) @ ${_me} and ignored to run job/steps" 1>&2
   exit 0
 }
 
 touch ${STATUS_JOB_RUNNING}
-sDir=$(dirname $0)
 
-echo "sDir: ${sDir}-----------------------"
-#-------------------
+sDir=$(dirname $0)
 for s in `ls ${sDir}/scripts.d/*-pre*.sh | sort -f`
 do
-   echo "${s} found then kick"
    ${s}
 done
 echo "${_me} done #########################"
